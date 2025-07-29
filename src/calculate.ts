b036deb300105bf3af79d26af4416cf619ac05b6
joshiawesome <dfelips@gmail.com>
2025-07-29 13:31:40 +0530

    case2: single number string handling

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,6 +1,10 @@
 class StringCalculator {
   public static add(numbers: string) {
     if (!numbers.length) return 0;
+
+    const numArray = numbers.split(",").map(Number);
+
+    if (numArray.length === 1) return numArray[0];
   }
 }
 
--- src/tests/add.test.ts
+++ src/tests/add.test.ts
@@ -4,4 +4,8 @@ describe("Add Method", () => {
   it("should return 0 when the input is empty", () => {
     expect(StringCalculator.add("")).toBe(0);
   });
+
+  it("should return the number when the input is a single number", () => {
+    expect(StringCalculator.add("1")).toBe(1);
+  });
 });
